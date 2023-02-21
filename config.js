b96ec45c44b26ff6212de03a74fb387797344b73
Sangit91 <125936723+Sangit91@users.noreply.github.com>
2023-02-21 16:26:12 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100005718317766';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Kiều Cute';
+const contentCard = 'Chúc em, cô gái xinh đẹp 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+
+
+
+const giftImage = 'hot-girl2.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
